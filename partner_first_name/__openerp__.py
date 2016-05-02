3fb1bb85d47c54981ab442aec209d2acee86ea0e
jobiols <jorge.obiols@gmail.com>
2016-05-01 21:10:20 -0300

    [REF] migrando a nueva api

--- partner_first_name/__openerp__.py
+++ partner_first_name/__openerp__.py
@@ -38,7 +38,7 @@ Fue creado para tener el nombre de pila disponible cuando se envia correo masivo
     'data': [
     ],
     'test': [
-        'tests/test_first_name'
+        'tests/test_first_name.py'
     ],
     'installable': True,
     'application': True,
