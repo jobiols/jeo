1d9b8ac2ea62c96fdb6f47747282173b3009d2b4
jobiols <jorge.obiols@gmail.com>
2016-06-25 19:34:45 -0300

    [ADD] corrección facturas...

--- valente_default/docs/patch1.sql
+++ valente_default/docs/patch1.sql
@@ -1,5 +1,3 @@
-/* borrar factura 40 y su nota de crédito */
-
 SELECT *
 FROM account_invoice
 WHERE afip_document_number = '0005-00000040';
@@ -25,26 +23,28 @@ SELECT *
 FROM account_move_line
 WHERE move_id = 649;
 
-
+/* borrar factura 40 y lineas de factura */
 DELETE FROM account_invoice_line
 WHERE invoice_id = 402;
 DELETE FROM account_invoice
 WHERE afip_document_number = '0005-00000040';
+/* borrar nota de crédito y lineas */
 DELETE FROM account_invoice
 WHERE afip_document_number = '0005-00000003';
 DELETE FROM account_invoice_line
 WHERE invoice_id = 403;
+/* borrar movimientos contables de factura 40 y lineas */
 DELETE FROM account_move
 WHERE afip_document_number = '0005-00000040';
 DELETE FROM account_move_line
 WHERE move_id = 648;
+/* borrar movimientos contables de factua 3 y lineas */
 DELETE FROM account_move
 WHERE afip_document_number = '0005-00000003';
 DELETE FROM account_move_line
 WHERE move_id = 649;
 
 
-/* Borrar factura 417 y su nota de crédito */
 SELECT *
 FROM account_invoice
 WHERE afip_document_number = '0005-00000417';
@@ -70,20 +70,24 @@ SELECT *
 FROM account_move_line
 WHERE move_id = 647;
 
+
+/* borrar factura 417 y sus lineas */
 DELETE FROM account_invoice
 WHERE afip_document_number = '0005-00000417';
 DELETE FROM account_invoice_line
 WHERE invoice_id = 400;
-DELETE FROM account_move
-WHERE afip_document_number = '0005-00000417';
-DELETE FROM account_move_line
-WHERE move_id = 646;
+/* borrar nota de crédito 15 y sus lineas */
 DELETE FROM account_invoice
 WHERE afip_document_number = '0005-00000015';
 DELETE FROM account_invoice_line
 WHERE invoice_id = 401;
+/* borrar movimientos contables de 417 y sus lineas */
+DELETE FROM account_move
+WHERE afip_document_number = '0005-00000417';
+DELETE FROM account_move_line
+WHERE move_id = 646;
+/* borrar movimientos contables de 15 y sus lineas */
 DELETE FROM account_move
 WHERE afip_document_number = '0005-00000015';
 DELETE FROM account_move_line
 WHERE move_id = 647;
-
