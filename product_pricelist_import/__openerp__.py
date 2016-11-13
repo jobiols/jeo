8dae33719183003c58820d29ee96241d55adc067
jobiols <jorge.obiols@gmail.com>
2016-11-13 09:13:35 -0300

    [DOC] product_pricelist_import mejorar doc

--- product_pricelist_import/__openerp__.py
+++ product_pricelist_import/__openerp__.py
@@ -32,10 +32,15 @@ Importar listas de precios
 ==========================
 
 Este módulo permite importar una lista de precios desde un archivo que puede ser csv
-o xls, el archivo tiene que tener las siguientes columnas:
+o xls, el archivo tiene que tener las siguientes columnas.
 
-product_code, product_name, list_price, package_qty, package_uom, categ, sub_categ,
+Formato del archivo de importación
+----------------------------------
+**Columnas:** product_code, product_name, list_price, [package_qty,] [package_uom,] categ, sub_categ,
 dp, dc1, dc2, ds1, ds2, ds3
+
+Nota:las que estan entre corchetes son opcionales las seis últimas columnas definen su
+comportamiento con los dos primeros caracteres del nombre.
 
 **product_code** Es el código del producto, debe ser único y si no lo es odoo generará una
 excepción, Esto lo maneja el módulo product_unique_default_code que esta como dependencia.
@@ -45,23 +50,32 @@ excepción, Esto lo maneja el módulo product_unique_default_code que esta como 
 **list_price** Es el precio de lista del proveedor, el precio al que el proveedor nos sugiere
 vender al público, sobre este precio el proveedor nos hace una serie de descuentos.
 
-**package_qty** Indica la cantidad de producto que hay en una caja sirve para aclarar en
+**package_qty** *Opcional* Indica la cantidad de producto que hay en una caja sirve para aclarar en
 la factura la cantidad total de producto que se vende dada la cantida de cajas vendidas.
 
-**package_uom** Indica la unidad de medida del producto que está en la caja.
+**package_uom** *Opcional* Indica la unidad de medida del producto que está en la caja.
 
-Las últimas seis lineas son los descuentos, los dos primeros caracteres del nombre de estas
+Las últimas seis columnas son los descuentos, los dos primeros caracteres del nombre de estas
 columnas indican donde va a aplicarse el descuento.
 - dp = descuento en el producto
 - dc = descuent en la categoría
 - ds = descuento en la sub categoría
 
-
-
-
 En el ejemplo se pusieron dos descuentos para la categoría y tres para la sub categoría
 pero se puede cambiar por ejemplo: dc1, ds1, ds2, ds3, ds4 en este caso tendríamos un
-descuento para la categoría y cuatro para la sub categoría.
+descuento para la categoría y cuatro para la sub categoría. El resto del nombre puede ser
+cualquier cosa, se estila poner un número que sería el orden de las columnas.
+
+Configuración
+-------------
+**Proveedores** Cada proveedor que va a enviar una lista de precios debe tener cargada
+una categoría, esa categoría representa todos los productos del proveedor. Se estila ponerle
+un nombre que tenga que ver con el nombre del proveedor en cuestion.
+
+
+
+Forma de Uso
+------------
 
 Al importar se debe seleccionar el nombre del proveedor y definir si se agregan productos
 o solo se actualizan los que ya están. El proveedor tiene que tener cargada una categoría
