b358e934dc9fd92831cb8c15822f4335a38f967c
jobiols <jorge.obiols@gmail.com>
2017-10-03 13:06:31 -0300

    [FIX] correccion permisos admin

--- vertical_ceramicas/__openerp__.py
+++ vertical_ceramicas/__openerp__.py
@@ -29,37 +29,51 @@
 Customización mayorista de ceramicas
 ====================================
 
-#Definicion de cuatro grupos de usuarios
+- Parametro adicional m2 o m por caja en producto
+- Si se usan descuentos, el Comercial solo pueden poner descuentos hasta 10%
+- Listas de precios restringidas, solo las ven Administrador y Encargado
+- Definicion de remitos para envios o para retiro en sucursal
+- Definicion de tres columnas configurables en listado de productos para mostrar listas de precio.
+- Rutas para definir envios o retiros desde el presupuesto.
+
+Definicion de cuatro grupos de usuarios
+---------------------------------------
 - Administrador
 - Encargado
 - Comercial
 - Almacen
 
-- Parametro adicional m2 o m por caja en producto
-- Si se usan descuentos, el Comercial solo pueden poner descuentos hasta 10%
-- Listas de precios restringidas, solo las ven Administrador y Encargado
-- Definicion de rutas para envios desde cualquier sucursal
-- Definicion de remitos para envios o para retiro en sucursal
-- Definicion de tres columnas configurables en listado de productos para mostrar listas de precio
-- Rutas para definir envios o retiros
-
-#Se usan las restricciones de los stores
+Se usan las restricciones de los stores
+---------------------------------------
 - Los usuarios de almacen tienen restrignidos los almacenes y diarios.
 - Los usuarios comerciales tienen restringidos diarios.
 
+Definicion de rutas
+-------------------
+Se requiere definir para cada almacen una ubicacion Envio y dos rutas, suponiendo un almacen WH las
+rutas tendrian la siguiente forma:
 
+**Ruta WH** genera un remito para que lo vengan a retirar al almacen
+
+WH:Stock -> Ubicaciones Clientes
+
+**Ruta WHE** genera un remito para que lo envien a la direccion de envio del cliente
+
+WHE:Stock -> Envios (Método de abastecimiento = Obtener de las existencias)
+
+WHE:Envios -> Ubicaciones Clientes (Método de abastecimiento = crear abastecimiento)
 
 """,
     'author': 'jeo Software',
     'depends': [
         'product',
         'base',
-        'base_multi_store',
-        'stock_multi_store',
         'account',
         'stock',
         'purchase',
-        'account_multi_store', 'base_multi_store', 'stock_multi_store',  # capacidad de limitar los diarios segun los stores (repo journal-constraint)
+        'account_multi_store',    # capacidad de limitar los diarios para los usuarios comerciales
+        'stock_multi_store',      # capacidad de limitar los diarios para los usuarios de almacen
+        'procurement_jit_stock',  # Planificación 'Just in time' (bajo demanda) con inventario
     ],
     'data': [
         'security/security_groups.xml',
